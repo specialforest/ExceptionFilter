3819f8ec04e20b1c0592db9e8f63f2fcb91cc046
Igor Shishkin <akaiten@mail.ru>
2012-04-02 21:45:04 +0400

    Added project to test rules editing form

---
+++ TestForm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ExceptionFilter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            RuleList rules = new RuleList();
+            List<RuleFactory> ruleFactories = new List<RuleFactory>();
+            ruleFactories.Add(new SimpleRuleFactory(typeof(ByFileRule), typeof(ExpressionRuleData)));
+            ruleFactories.Add(new SimpleRuleFactory(typeof(ByFuncRule), typeof(ExpressionRuleData)));
+            Application.Run(new RulesForm(rules, ruleFactories));
+        }
+    }
+}
