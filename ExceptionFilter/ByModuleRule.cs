08c7d8895fe671d0c879ab47f0e453f23d37a92e
Igor Shishkin <akaiten@mail.ru>
2012-04-05 23:00:17 +0400

    Added by module exception filter rule

---
+++ ExceptionFilter/ByModuleRule.cs
@@ -0,0 +1,63 @@
+﻿namespace ExceptionFilter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel;
+    using System.Xml.Serialization;
+
+    public class ModuleRuleData : RuleData
+    {
+        [DefaultValue("")]
+        [XmlElementAttribute("Module")]
+        public string Module { get; set; }
+    }
+
+    [Description("Module Filter")]
+    [RuleType("ModuleFilter")]
+    public class ByModuleRule : Rule
+    {
+        private ModuleRuleData data;
+
+        public ByModuleRule()
+        {
+            this.data = new ModuleRuleData();
+        }
+
+        public ByModuleRule(RuleData data)
+        {
+            this.data = (ModuleRuleData)data;
+        }
+
+        public override RuleData Data
+        {
+            get { return this.data; }
+        }
+
+        public override ExceptionHandler CreateHandler()
+        {
+            return new ByModuleExceptionHandler(data);
+        }
+    }
+
+    public class ByModuleExceptionHandler : ExceptionHandler
+    {
+        private ModuleRuleData data;
+
+        public ByModuleExceptionHandler(ModuleRuleData data)
+        {
+            this.data = data;
+        }
+
+        public override ExceptionAction? Handle(EnvDTE.StackFrame frame)
+        {
+            if (frame.Module == this.data.Module)
+            {
+                return this.data.Action;
+            }
+
+            return null;
+        }
+    }
+}
